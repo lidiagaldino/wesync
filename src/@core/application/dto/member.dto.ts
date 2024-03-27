f1861a9b50b832c23480d387c960da5f2c729531
lidiagaldino <lidiagabrielly14@gmail.com>
2024-03-27 16:34:21 -0300

    feat(dto): Add input and output DTOs for Member

---
+++ src/@core/application/dto/member.dto.ts
@@ -0,0 +1,12 @@
+export type TInputMemberDTO = {
+  user_id: number;
+  party_id: number;
+  is_admin: boolean;
+};
+
+export type TOutputMemberDTO = {
+  id: number;
+  user_id: number;
+  party_id: number;
+  is_admin: boolean;
+};
