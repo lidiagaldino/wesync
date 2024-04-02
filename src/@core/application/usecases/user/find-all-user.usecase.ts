41e6aa02d7ef5e57e3027e86752fb7313e94683c
lidiagaldino <lidiagabrielly14@gmail.com>
2024-04-02 13:15:49 -0300

    fix: Handle case when user is not found in FindAllUserUsecase
    This commit adds error handling in the FindAllUserUsecase to throw a NotFoundException when no user is found. This prevents potential errors and provides better feedback to the client.

--- src/@core/application/usecases/user/find-all-user.usecase.ts
+++ src/@core/application/usecases/user/find-all-user.usecase.ts
@@ -1,4 +1,5 @@
 import { IUserRepository } from '../../../domain/repositories/user.repository';
+import { NotFoundException } from '../../../domain/shared/errors/not-found.exception';
 import { TOutputUserDTO } from '../../dto/user.dto';
 import { mapOutput } from './map';
 
@@ -7,6 +8,7 @@ export class FindAllUserUsecase {
 
   async execute(): Promise<TOutputUserDTO[]> {
     const user = await this.userRepository.findAll();
-    return user.map(mapOutput);
+    if (user) return user.map(mapOutput);
+    throw new NotFoundException('USER_NOT_FOUND');
   }
 }
