615a489f7d6e424f46cea95672d9329365be75cb
lidiagaldino <lidiagabrielly14@gmail.com>
2024-04-02 11:54:07 -0300

    refactor: Updated IUserRepository interface to use Password value object
    This commit refactors the IUserRepository interface to use the Password value object for the changePassword method instead of accepting a string directly.

--- src/@core/domain/repositories/user.repository.ts
+++ src/@core/domain/repositories/user.repository.ts
@@ -1,15 +1,16 @@
-import { User } from "../entities/user.entity";
-import { Email } from "../value-objects/email.value-object";
+import { User } from '../entities/user.entity';
+import { Email } from '../value-objects/email.value-object';
+import { Password } from '../value-objects/password.value-object';
 
 /**
  * Interface for the user repository.
  */
 export interface IUserRepository {
-    create(user: User): Promise<User>
-    update(user: User): Promise<User>
-    changePassword(newPassword: string, user: User): Promise<User>
-    delete(user: User): Promise<void>
-    findById(id: number): Promise<User>
-    findByEmail(email: Email): Promise<User>
-    findAll(): Promise<User[]>
+  create(user: User): Promise<User>;
+  update(user: User): Promise<User>;
+  changePassword(newPassword: Password, user: User): Promise<User>;
+  delete(user: User): Promise<void>;
+  findById(id: number): Promise<User>;
+  findByEmail(email: Email): Promise<User>;
+  findAll(): Promise<User[]>;
 }
