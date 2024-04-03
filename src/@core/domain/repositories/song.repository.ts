69302ed547f90cc31b970575e8a9055ba755c9c7
lidiagaldino <lidiagabrielly14@gmail.com>
2024-04-03 17:46:19 -0300

    feat: Add findByUrl method to ISongRepository

--- src/@core/domain/repositories/song.repository.ts
+++ src/@core/domain/repositories/song.repository.ts
@@ -1,14 +1,16 @@
-import { Genre } from "../entities/genre.entity";
-import { Song } from "../entities/song.entity";
+import { Genre } from '../entities/genre.entity';
+import { Song } from '../entities/song.entity';
+import { Url } from '../value-objects/url.value-object';
 
 /**
  * Interface for the song repository.
  */
 export interface ISongRepository {
-    create(song: Song): Promise<Song>
-    update(song: Song): Promise<Song>
-    delete(song: Song): Promise<void>
-    findById(id: number): Promise<Song>
-    findAll(): Promise<Song[]>
-    findByGenre(genre: Genre): Promise<Song[]>
+  create(song: Song): Promise<Song>;
+  update(song: Song): Promise<Song>;
+  delete(song: Song): Promise<void>;
+  findById(id: number): Promise<Song>;
+  findAll(): Promise<Song[]>;
+  findByGenre(genre: Genre): Promise<Song[]>;
+  findByUrl(url: Url): Promise<Song>;
 }
