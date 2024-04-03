b0a17f489e54f457bfcf075972c589542f6cd1f6
lidiagaldino <lidiagabrielly14@gmail.com>
2024-04-03 17:47:52 -0300

    feat: Implement mapOutput function for UserSong entity
    This commit adds the mapOutput function, which transforms a UserSong entity into a TOutputUserSongDTO object.

---
+++ src/@core/application/usecases/user-song/map.ts
@@ -0,0 +1,14 @@
+import { UserSong } from '../../../domain/entities/user-song.entity';
+import { TOutputUserSongDTO } from '../../dto/user-song.dto';
+
+export const mapOutput = (data: UserSong): TOutputUserSongDTO => {
+  return {
+    id: data.getId(),
+    user_id: data.getUser().getId(),
+    url: data.getSong().getUrl().getUrl(),
+    customName: data.getCustomName(),
+    photo: data.getPhoto().getUrl(),
+    isPublic: data.getIsPublic(),
+    isFavorite: data.getIsFavorite(),
+  };
+};
